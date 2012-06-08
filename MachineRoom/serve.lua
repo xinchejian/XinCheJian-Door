5a3ddb3aa6ba0a29cebbdfe258a92d9067bd15aa
Lionello Lunesu <lio@lunesu.com>
2012-06-08 08:19:16 +0800

    Adding assert to io.open; replaced tabs with spaces

--- MachineRoom/serve.lua
+++ MachineRoom/serve.lua
@@ -1,8 +1,8 @@
 
 local function setled(i)
-    local f = io.open("/sys/class/leds/tp-link\:blue\:system/brightness", "w")
+    local f = assert(io.open("/sys/class/leds/tp-link\:blue\:system/brightness", "w"))
     f:write(i)
-	f:close()
+    f:close()
 end
 
 local function lock() setled(0) end
@@ -28,30 +28,30 @@ local function main ()
   server:settimeout(30)
   local client, err = server:accept()
   if err then
-	-- accept timed out - close the door
+    -- accept timed out - close the door
     lock()
   else
-	-- make sure we don't block waiting for this client's line
-	client:settimeout(1)
-	-- receive the lines
-	local firstLine = Nil
-	while 1 do
-	  local line, err = client:receive()
-	  if not firstLine then firstLine = line end
-	  if line == "" then break end
-	  if err then break end
-	end
-	-- if there was no error, send it back to the client
-	if firstLine == "GET /lock?pin=0326 HTTP/1.1" then
-	  -- open the door
-	  unlock()
-	  client:send(header .. "Opened" .. footer)
-	else
-	  client:send(header ..
-		"<form method='GET' action='lock'>PIN <input id='pin' name='pin' type='password' maxlength='4' size='4'/> <input type='submit' value='Unlock'/></form>"..footer)
-	end
-	-- done with client, close the object
-	client:close()
+    -- make sure we don't block waiting for this client's line
+    client:settimeout(1)
+    -- receive the lines
+    local firstLine = Nil
+    while 1 do
+      local line, err = client:receive()
+      if not firstLine then firstLine = line end
+      if line == "" then break end
+      if err then break end
+    end
+    -- if there was no error, send it back to the client
+    if firstLine == "GET /lock?pin=0326 HTTP/1.1" then
+      -- open the door
+      unlock()
+      client:send(header .. "Opened" .. footer)
+    else
+      client:send(header ..
+        "<form method='GET' action='lock'>PIN <input id='pin' name='pin' type='password' maxlength='4' size='4'/> <input type='submit' value='Unlock'/></form>"..footer)
+    end
+    -- done with client, close the object
+    client:close()
   end
 end
 
