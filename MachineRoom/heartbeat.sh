2d537058b1eab23268ff84360c9cb92053706615
Paul <nihaopaul@gmail.com>
2012-07-05 00:19:54 +0800

    the last script did not account for the interface not returning anything if it was down.

--- MachineRoom/heartbeat.sh
+++ MachineRoom/heartbeat.sh
@@ -1,31 +1,23 @@
 #!/bin/sh
 # To be setup as a cron job to check network connectivity and reboot the server when necessary.
-#
-# Cron string
-#m	h	dom	mon	dow	command
-#*/5	*	*	*	*	/usr/bin/heartbeat.sh
-
 touch /root/heartbeat.lastrun
-                             
+
 REBOOTLOG=/root/reboot.log
-                                                              
+
 GATEWAY=`route|grep 'default'|sed 's/  */ /g'|cut -d ' ' -f 2`
-                                                                  
-NETWORKDOWN=`ping -qc 3 $GATEWAY |grep '0 packets received'|wc -l`
-                             
-                                        
-# if network is down, reboot the router.
-if [ $NETWORKDOWN == 1 ]; then
-                                                 
+NETWORKDOWN=`ping -c 3 10.0.10.5 | grep '64 bytes' | wc -l`
+
+#if network is down, reboot the router.
+if [ $NETWORKDOWN -lt 1 ]; then
+
   # trim the reboot log file if it is getting big
   if [ `wc -l < $REBOOTLOG` -ge 3 ]; then
-    sed -i '1d' $REBOOTLOG
+    /bin/sed -i '1d' $REBOOTLOG
   fi
-                           
-  # log the rebooting event                      
+  
+  # log the rebooting event
   echo "Lost connection at " `date` >> $REBOOTLOG
-                     
+  
   # Reboot the router
-  reboot
+  /sbin/reboot
 fi
-
