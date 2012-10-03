5028e5df0a6b515440468f62e7af47debfe06da5
Lionello Lunesu <lionello@lunesu.com>
2012-10-03 16:59:25 +0800

    updated the deployment script

--- MachineRoom/deploy.sh
+++ MachineRoom/deploy.sh
@@ -2,6 +2,13 @@ echo 'Copying the door http program ...'
 chmod a+x serve.lua
 scp serve.lua root@10.0.10.9:/usr/bin/
 
+echo 'Copying arp ...'
+chmod a+x arp
+scp arp root@10.0.10.9:/usr/bin/
+
+echo 'Copying tts-token ...'
+scp tts-token root@10.0.10.9:/usr/bin/
+
 echo 'Copy the door init service script ..'
 chmod a+x door
 scp door root@10.0.10.9:/etc/init.d/
